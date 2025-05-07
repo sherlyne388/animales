8ac8cf25e5af7ad3f173af24474f85811fb5ebd1
sherlyne388 <oscamila11@gmail.com>
2025-05-07 13:46:21 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+import './App.css'
 
-function App() {
+import AnimalesShow from './AnimalesShow'
 
+function getRandomAnimal(){
+  const animales = ['cat', 'bird', 'cow', 'dog', 'gator', 'horse']
 
-
-  return (
-    <>
-     <h1>Animals</h1>
-    </>
-  )
+  return animales[Math.floor(Math.random() * aniamles.length)]
 }
 
-export default App
+function App(){
+  const [animales, setAnimales] = useState([])
+
+    const handleClick = ( ) =>{
+      setAnimales ([...animales, getRandomAnimal()])
+
+    }
+
+    const renderedAnimales = animales.map((animal, index) => {
+      return <animalesShow type = {animal} key = {index} />
+    })
+
+    return (
+      <div className='app'>
+        <button onClick={handleClick}>Add Animal</button>
+        <div className="animales-list">
+          {renderedAnimales}
+        </div>
+      </div>
+    )
+}
